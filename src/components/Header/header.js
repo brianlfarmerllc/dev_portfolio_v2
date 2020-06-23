2f01680b8af97d5d8e5876bd0b87d2b0fc81bf19
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-06-22 22:45:51 -0500

    added header.js component and header.css

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React from "react";
+import "./header.css"
 
 
 function Header () {
     return (
-        <>
-        </>
+        // header container 
+  <section className="container-fluid">
+    <header className="row justify-content-around mr-0 mr-lg-5" id="header">
+      {/* h1 element on the top left of the page */}
+      <div className="col-12 col-md-3">
+        <h1><span style={{fontFamily: "Merriweather, serif"}}>Bio</span> <span
+            style={{fontFamily:"Source Sans Pro, sans-serif", fontSize: "xx-large"}}> Sheet</span></h1>
+      </div>
+      {/* top navigation bar styled to looks like and array */}
+      <div className="col-12 col-md-9 col-xl-7">
+        <nav className="nav justify-content-md-end justify-content-center">
+          <span>var navBar = [</span>
+          <a className="nav-link" href="#aboutMe">About Me</a>
+          <a className="nav-link" href="#portfolioSection">My Portfolio</a>
+          <a className="nav-link" href="#contactSection">Contact Me</a>
+          <span>]</span>
+        </nav>
+      </div>
+    </header>
+  </section>
     )
 }
 
