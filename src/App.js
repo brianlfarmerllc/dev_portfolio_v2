0cacbab5d1119c545b99da7898227e920d176f27
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-06-23 08:55:21 -0500

    created main element for syntax purposes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Header from "./components/Header/Header"
 import Hero from './components/Hero/Hero';
+import Main from './components/Main/Main';
 
 
 function App() {
   return (
     <>
    <Header/>
+   <Main>
    <Hero/>
+   </Main>
    </>
   );
 }
---
+++ src/components/Main/Main.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Main() {
+    return <main></main>
+}
+
+export default Main
