61a526cee3e5eb4aa41c8c3a6c3e89310838bf1a
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-06-24 15:02:53 -0500

    moved mapping to Portfolio from Portfolio tiles and passed props to Portfolio Tiles

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,8 +1,13 @@
-import React from "react"
+import React,{useEffect, useState, useReducer} from "react"
+import portfolio from "../../db/portfolio.json"
 import "./Portfolio.css"
 import PortfolioTiles from "../PortfolioTiles/PortfolioTiles"
 
 function Portfolio() {
+    const [portfolioState, setPortfolioState] = useState([]);
+
+
+
     return (
         // section for portfolio
         <section className="container-fluid" id="portfolioSection">
@@ -18,17 +23,28 @@ function Portfolio() {
             <div className="row justify-content-center ml-1 mr-1 ml-m-0 mr-m-0">
                 {/* line of text above the icons */}
                 <ul className="nav justify-content-md-end justify-content-center">
-                    <span style={{paddingRight: "1em"}}>var navBar = [</span>
-                    <li> <a className="list-item" href="#aboutMe">About Me</a> </li>
-                    <li> <a className="list-item" href="#portfolioSection">My Portfolio</a> </li>
-                    <li> <a className="list-item" href="#contactSection">Contact Me</a> </li>
-                    <span style={{paddingLeft: "1em"}}>]</span>
+                    <span>var portfolio = [</span>
+                    <li> <a className="list-item" href="#">Full Stack</a> </li>
+                    <li> <a className="list-item" href="#">Browser Apps</a> </li>
+                    <li> <a className="list-item" href="#">CLI Apps</a> </li>
+                    <span >]</span>
                 </ul>
             </div>
             <div className="row justify-content-center text-center ml-0 mr-0 ml-xl-2 mr-xl-2">
                 {/* Force next columns to break to new line */}
                 <div className="w-100 d-none d-md-block"></div>
-                <PortfolioTiles />
+                {portfolio.map(item => (
+                <PortfolioTiles 
+                key={item.name}
+                name={item.name}
+                image={item.image}
+                imageAlt={item.imageAlt}
+                site={item.site} 
+                icon={item.icon}
+                iconAlt={item.iconAlt}
+                type={item.type}
+                github={item.github}/>
+                ))}
             </div>
         </section>
     )
--- src/components/PortfolioTiles/PortfolioTiles.js
+++ src/components/PortfolioTiles/PortfolioTiles.js
@@ -1,24 +1,29 @@
-import React from "react"
+import React,{useEffect, useState, useReducer} from "react"
 import portfolio from "../../db/portfolio.json"
 
-function PortfolioTiles() {
+
+
+function PortfolioTiles(props) {
+    
+   
+
     return (
         <>
-            {portfolio.map(item => (
-                <div key={item.name} className="col-10 col-sm-8 col-md-5 col-lg-3 pr-0 pl-0 mt-5 tile" style={{ position: "relative"}}>
-                    <img src={item.image} alt={item.imageAlt} style={{ width: "100%" }} />
+            
+                <div key={props.name} className="col-10 col-sm-8 col-md-5 col-lg-3 pr-0 pl-0 mt-5 tile" style={{ position: "relative"}}>
+                    <img src={props.image} alt={props.imageAlt} style={{ width: "100%" }} />
                     <div className="box">
-                        <a href={item.site} target="blank">
-                            <h5 style={{ textAlign: "center" }}>{item.name}</h5>
+                        <a href={props.site} target="blank">
+                            <h5 style={{ textAlign: "center" }}>{props.name}</h5>
                         </a>
-                        <img src={item.icon} alt={item.iconAlt} className="tileIcon text-center" />
-                        <p>{item.type}</p>
-                        <a href={item.github} target="blank">
+                        <img src={props.icon} alt={props.iconAlt} className="tileIcon text-center" />
+                        <p>{props.type}</p>
+                        <a href={props.github} target="blank">
                             <h5 style={{ textAlign: "center" }}>View GitHub</h5>
                         </a>
                     </div>
                 </div>
-            ))}
+            
         </>
     )
 }
