3750b8d5b2249b33115394e94c560895ddff798d
brianlfarmerllc <brianlfarmerllc@gmail.com>
2020-06-25 19:06:22 -0500

    Updates

---
+++ precache-manifest.654c551407d6930a426e73d3a239e137.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13ac46df2213a35c64b1bceda587e6df",
+    "url": "/No_Express_React_Portfolio/index.html"
+  },
+  {
+    "revision": "c0b67bc5626614450272",
+    "url": "/No_Express_React_Portfolio/static/css/main.4528dbf2.chunk.css"
+  },
+  {
+    "revision": "5e9069530416c0ff96e0",
+    "url": "/No_Express_React_Portfolio/static/js/2.28ffda63.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/No_Express_React_Portfolio/static/js/2.28ffda63.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0b67bc5626614450272",
+    "url": "/No_Express_React_Portfolio/static/js/main.3eb1b421.chunk.js"
+  },
+  {
+    "revision": "5690bf56df131d422bfe",
+    "url": "/No_Express_React_Portfolio/static/js/runtime-main.b63d8624.js"
+  }
+]);
