3d294533c81da35dd4c2d09d1728071d1c5e734d
Brian Farmer <brianlfarmerllc@gmail.com>
2020-07-16 17:34:09 -0500

    testing out netlify form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -16,26 +16,28 @@ function Contact() {
       {/* eslint-disable-next-line jsx-a11y/heading-has-content */}
       <h4 id="thankYou" style={{textAlign: "center", fontFamily: "'Merriweather', serif"}}></h4>
       <div className="row justify-content-center" id="formRow">
-        <form className="col-sm-8 col-md-8 col-lg-6" id="contactForm">
+        <form name="contact" method="post" className="col-sm-8 col-md-8 col-lg-6" id="contactForm">
+        {/* hidden input field */}
+        <input type="hidden" name="form-name" value="contact" />
           <div className="row form-group">
             <div className="col-6">
-              <input id="firstName" type="text" className="form-control" placeholder="Your First Name*"/>
+              <input name="firstName" id="firstName" type="text" className="form-control" placeholder="Your First Name*"/>
             </div>
             <div className="col-6">
-              <input id="lastName" type="text" className="form-control" placeholder="Your Last Name*"/>
+              <input name="lastName" id="lastName" type="text" className="form-control" placeholder="Your Last Name*"/>
             </div>
           </div>
           <div className="row form-group">
             <div className="col-6">
-              <input id="email" type="text" className="form-control" placeholder="Your Email*"/>
+              <input name="email" id="email" type="text" className="form-control" placeholder="Your Email*"/>
             </div>
             <div className="col-6">
-              <input id="phone" type="text" className="form-control" placeholder="Your Number*"/>
+              <input name="phone" id="phone" type="text" className="form-control" placeholder="Your Number*"/>
             </div>
           </div>
           <div className="row form-group">
             <div className="col-12">
-              <textarea id="body" type="text" className="form-control" rows="8" placeholder="Your Message*"></textarea>
+              <textarea name="message" id="body" type="text" className="form-control" rows="8" placeholder="Your Message*"></textarea>
             </div>
           </div>
           <button id="contactSubmit" type="submit" className="button">Submit</button>
