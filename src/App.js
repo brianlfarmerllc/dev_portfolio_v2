f2135c04bcb90efd9a83f6101ef555fc43ea879c
Brian Farmer <brianlfarmerllc@gmail.com>
2020-07-16 16:48:05 -0500

    changed file path in APP.js header

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from "./components/Header/Header"
+import Header from "./components/Header/Header.js"
 import Main from './pages/Main/Main';
 import Footer from "./components/Footer/Footer"
 
