8fda7ad51fc417b88469110ffcb14617f28a799c
brianlfarmerllc <brianlfarmerllc@gmail.com>
2021-06-18 12:50:48 -0500

    updated readme information for new portfolio

--- README.md
+++ README.md
@@ -1,60 +1,149 @@
+# Personal Portfolio Version 2
 
-  # No_Express_React_Portfolio
+This site my personal Bio/Portfolio - It is an ever-changing work in progress as I continue to work towards developing my technical skills.
 
-  [![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)](https://github.com/brianlfarmerllc/No_Express_React_Portfolio)
-  [![GitHub URL](https://img.shields.io/badge/Netlify-URL-pink.svg)](https://brianlfarmerllc-biosite.netlify.app/)
-  [![Netlify Status](https://api.netlify.com/api/v1/badges/d5ae00f7-be90-401b-ae52-1564e2d874ef/deploy-status)](https://app.netlify.com/sites/brianlfarmerllc-biosite/deploys)
+The page is built with React.js. Currently the site consists of a single page layout with divided into 4 main sections. Above the fold, About Me, Portfolio, and Contact. A navigation bar anchors to the corresponding sections of the page.
 
-  
-  ## Description
+## Table of contents
 
-  This site my personal Bio/Portfolio - It is an ever-changing work in progress as I continue to work towards developing my technical skills.
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
 
-  The page is built with React.js. Currently the site consists of a single page layout with divided into 4 main sections. Above the fold, About Me, Portfolio, and Contact. A navigation bar anchors to the corresponding sections of the page.  
+## Overview
 
-  ## Table of Contents
-  
-  * [Installation](#Installation)
-  * [Test](#Test)
-  * [Usage](#Usage)
-  * [Technologies](#Technologies)
-  * [License](#License)
-  * [Contributions](#Contributions)
-  
-  ## Installation
+### The challenge
 
-  Clone repository to your local machine then run npm install. To run on local server, "npm start" and vist site at localhost:3000. Feel free to use the site as a template to build your own portfolio! 
+Users should be able to:
 
-  ## Test
+- Navigate the site in desktop and mobile views
+- Use the navigation menu to move to the corresponding section of the page
+- View resume link through google docs
+- Toggle through the different portfolio sections and generate portfolio cards for section
+- Send a contact form to netlify forms
 
-  No test
+### Screenshot
 
-  ## Usage
+![](./screenshot.png)
 
-  The following image demonstrates the application functionality:
+### Links
 
-  ![Add Description !!!!!!](./public/bioPage.gif)
+- Git Hub URL: [https://github.com/brianlfarmerllc/dev_portfolio_v2](https://github.com/brianlfarmerllc/dev_portfolio_v2)
+- Live Site URL: [https://brianlfarmerllc-biosite.netlify.app/](https://brianlfarmerllc-biosite.netlify.app/)
 
-  ## Technologies
+## My process
 
-  The following technologies were used in the developement of the application
+### Built with
 
-  React,<br>HTML5,<br>CSS,<br>Javascript and jQuery,<br>Bootstrap,<br>Node.js,<br>
+- Semantic HTML5 markup
+- React.js
+- JSX Functionality
+- React Hook State Management
+- Bootstrap 4
+- JSON DB
 
-  ## License
+### What I learned
 
-  This application is licensed under the MIT license
+One thing I really wanted to accomplish was to have the ability to manipulate the virtual DOM in a specific section of the page. I was able to accomplish this in the portfolio section by allowing the user to select from a navigation menu what type of project they wanted to view and the content in that section would update.
 
-  ## Contributors
+I created a db.json file where I constructed my project information similar to the format below
 
-  Contributor information and commit history can be seen here!
-  <a href="https://github.com/https://github.com/brianlfarmerllc/No_Express_React_Portfolio/graphs/contributors">
-    <img src="https://contributors-img.web.app/image?repo=brianlfarmerllc/No_Express_React_Portfolio" />
-  </a>
+```JSON
+[
+  {
+    "fullStack": [
+      {
+        "image": "burger",
+        "imageAlt": "background for eat-da-burger",
+        "site": "https://eat-da-burger-brianlfarmerllc.herokuapp.com/",
+        "name": "Eat-Da-Burger Application",
+        "icon": "https://img.icons8.com/material-outlined/48/000000/hamburger.png",
+        "iconAlt": "burger icon",
+        "type": "Full Stack App",
+        "github": "https://github.com/brianlfarmerllc/Handelbars_Eat_Da_Burger"
+      },
+      {
+        "image": "notebook",
+        "imageAlt": "background for notetaker",
+        "site": "https://note-taker-brianlfarmerllc.herokuapp.com/",
+        "name": "Note Taker Application",
+        "icon": "https://img.icons8.com/wired/64/000000/pencil.png",
+        "iconAlt": "pencil icon",
+        "type": "Full Stack App",
+        "github": "https://github.com/brianlfarmerllc/Note_Taker"
+      }
+    ]
+  }
+]
+```
 
-  Made with [contributors-img](https://contributors-img.web.app).
+Then I used an event handler to compare the name associated with the selected anchor tag and set the portfolio state to match. This one switch statement handles both the virtual dom manipulation and the changing of the color on the active anchor tag.
 
-  ## Questions
+```JSX
+const [portfolioState, setPortfolioState] = useState(portfoliocopy[3].reactApps)
+const [currentItemState, setCurrentItemState] = useState("React Apps");
 
-  If you have any questions about the application you can contact brianlfarmerllc directly @ brianlfarmerllc@gmail.com
-  
+    const handleClick = event => {
+        event.preventDefault()
+        const name = event.target.name
+        switch (name) {
+            case "React Apps":
+                setPortfolioState(portfoliocopy[3].reactApps)
+                setCurrentItemState("React Apps")
+                break;
+            case "Full Stack":
+                setPortfolioState(portfoliocopy[0].fullStack)
+                setCurrentItemState("Full Stack")
+                break;
+            case "Browser Apps":
+                setPortfolioState(portfoliocopy[1].browserApps)
+                setCurrentItemState("Browser Apps")
+                break;
+            case "CLI Apps":
+                setPortfolioState(portfoliocopy[2].cliApps)
+                setCurrentItemState("CLI Apps")
+                break;
+
+            default:
+                return
+        }
+    }
+```
+
+This one switch statement handles both the virtual dom manipulation and the changing of the color on the active anchor tag using a ternary operator on the anchors style attribute
+
+```jsx
+<a
+  className="list-item"
+  href="#"
+  name="React Apps"
+  style={{
+    color: currentItemState === "React Apps" ? "dodgerblue" : "black",
+  }}
+  onClick={handleClick}
+>
+  React Apps
+</a>
+```
+
+### Continued development
+
+This was a challenging project for me at the time with my current skill set using react, managing state, and using state as the single source of truth. I plan on using React in many more projects to progress with my understanding of all the available hooks.
+
+### Useful resources
+
+- [React Documentation](https://reactjs.org/docs/getting-started.html) - I learn a lot about React by just using their documentation. Well thought ought and put together information
+
+## Author
+
+- Website - [Brian Farmer](https://brianlfarmerllc-biosite.netlify.app/)
+- GitHub URL: - [GitHub URL](https://github.com/brianlfarmerllc/fem_launch_timer)
+- Frontend Mentor - [@brianlfarmerllc](https://www.frontendmentor.io/profile/brianlfarmerllc)
--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -1,102 +1,139 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { useState } from "react"
-import portfoliocopy from "../../db/portfolio copy.json"
+import React, { useState } from "react";
+import portfoliocopy from "../../db/portfolio copy.json";
 // import portfolio from "../../db/portfolio.json"
-import "./Portfolio.css"
-import PortfolioTiles from "../PortfolioTiles/PortfolioTiles"
-import dev from "../../assets/icons/developer.png"
+import "./Portfolio.css";
+import PortfolioTiles from "../PortfolioTiles/PortfolioTiles";
+import dev from "../../assets/icons/developer.png";
 
 function Portfolio() {
-    const [portfolioState, setPortfolioState] = useState(portfoliocopy[3].reactApps);
-    const [currentItemState, setCurrentItemState] = useState("React Apps");
+  const [portfolioState, setPortfolioState] = useState(
+    portfoliocopy[3].reactApps
+  );
+  const [currentItemState, setCurrentItemState] = useState("React Apps");
 
-    const handleClick = event => {
-        event.preventDefault()
-        const name = event.target.name
-        switch (name) {
-            case "React Apps":
-                setPortfolioState(portfoliocopy[3].reactApps)
-                setCurrentItemState("React Apps")
-                break;
-            case "Full Stack":
-                setPortfolioState(portfoliocopy[0].fullStack)
-                setCurrentItemState("Full Stack")
-                break;
-            case "Browser Apps":
-                setPortfolioState(portfoliocopy[1].browserApps)
-                setCurrentItemState("Browser Apps")
-                break;
-            case "CLI Apps":
-                setPortfolioState(portfoliocopy[2].cliApps)
-                setCurrentItemState("CLI Apps")
-                break;
+  const handleClick = (event) => {
+    event.preventDefault();
+    const name = event.target.name;
+    switch (name) {
+      case "React Apps":
+        setPortfolioState(portfoliocopy[3].reactApps);
+        setCurrentItemState("React Apps");
+        break;
+      case "Full Stack":
+        setPortfolioState(portfoliocopy[0].fullStack);
+        setCurrentItemState("Full Stack");
+        break;
+      case "Browser Apps":
+        setPortfolioState(portfoliocopy[1].browserApps);
+        setCurrentItemState("Browser Apps");
+        break;
+      case "CLI Apps":
+        setPortfolioState(portfoliocopy[2].cliApps);
+        setCurrentItemState("CLI Apps");
+        break;
 
-            default:
-                return
-        }
+      default:
+        return;
     }
+  };
 
-    return (
-        // section for portfolio
-        <section className="container-fluid" id="portfolioSection">
-            <div className="row justify-content-center">
-                <div className="col">
-                    <h2 style={{ fontFamily: "'Merriweather', serif" }}>Portfolio</h2>
-                </div>
-                <div className="w-100"></div>
-                <img alt="icon brackets" className="codingIcon"
-                    src={dev} />
-            </div>
+  return (
+    // section for portfolio
+    <section className="container-fluid" id="portfolioSection">
+      <div className="row justify-content-center">
+        <div className="col">
+          <h2 style={{ fontFamily: "'Merriweather', serif" }}>Portfolio</h2>
+        </div>
+        <div className="w-100"></div>
+        <img alt="icon brackets" className="codingIcon" src={dev} />
+      </div>
 
-            <div className="row justify-content-center ml-1 mr-1 ml-m-0 mr-m-0">
-                {/* line of text above the icons */}
-                <ul className="nav justify-content-md-end justify-content-center">
-                    <span>var portfolio = [</span>
-                    <li> <a
-                        className="list-item"
-                        href="#"
-                        name="React Apps"
-                        style={{ color: currentItemState === "React Apps" ? "dodgerblue" : "black" }}
-                        onClick={handleClick}>React Apps</a> </li>
-                    <li> <a
-                        className="list-item"
-                        href="#"
-                        name="Full Stack"
-                        style={{ color: currentItemState === "Full Stack" ? "dodgerblue" : "black" }}
-                        onClick={handleClick}>Full Stack</a> </li>
-                    <li> <a
-                        className="list-item"
-                        href="#"
-                        name="Browser Apps"
-                        style={{ color: currentItemState === "Browser Apps" ? "dodgerblue" : "black" }}
-                        onClick={handleClick}>Browser Apps</a> </li>
-                    <li> <a
-                        className="list-item"
-                        href="#"
-                        name="CLI Apps"
-                        style={{ color: currentItemState === "CLI Apps" ? "dodgerblue" : "black" }}
-                        onClick={handleClick}>CLI Apps</a> </li>
-                    <span >]</span>
-                </ul>
-            </div>
-            <div className="row justify-content-center text-center ml-0 mr-0 ml-xl-2 mr-xl-2">
-                {/* Force next columns to break to new line */}
-                <div className="w-100 d-none d-md-block"></div>
-                {portfolioState.map(item => (
-                    <PortfolioTiles
-                        key={item.name}
-                        name={item.name}
-                        image={item.image}
-                        imageAlt={item.imageAlt}
-                        site={item.site}
-                        icon={item.icon}
-                        iconAlt={item.iconAlt}
-                        type={item.type}
-                        github={item.github} />
-                ))}
-            </div>
-        </section>
-    )
+      <div className="row justify-content-center ml-1 mr-1 ml-m-0 mr-m-0">
+        {/* line of text above the icons */}
+        <ul className="nav justify-content-md-end justify-content-center">
+          <span>var portfolio = [</span>
+          <li>
+            {" "}
+            <a
+              className="list-item"
+              href="#"
+              name="React Apps"
+              style={{
+                color:
+                  currentItemState === "React Apps" ? "dodgerblue" : "black",
+              }}
+              onClick={handleClick}
+            >
+              React Apps
+            </a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a
+              className="list-item"
+              href="#"
+              name="Full Stack"
+              style={{
+                color:
+                  currentItemState === "Full Stack" ? "dodgerblue" : "black",
+              }}
+              onClick={handleClick}
+            >
+              Full Stack
+            </a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a
+              className="list-item"
+              href="#"
+              name="Browser Apps"
+              style={{
+                color:
+                  currentItemState === "Browser Apps" ? "dodgerblue" : "black",
+              }}
+              onClick={handleClick}
+            >
+              Browser Apps
+            </a>{" "}
+          </li>
+          <li>
+            {" "}
+            <a
+              className="list-item"
+              href="#"
+              name="CLI Apps"
+              style={{
+                color: currentItemState === "CLI Apps" ? "dodgerblue" : "black",
+              }}
+              onClick={handleClick}
+            >
+              CLI Apps
+            </a>{" "}
+          </li>
+          <span>]</span>
+        </ul>
+      </div>
+      <div className="row justify-content-center text-center ml-0 mr-0 ml-xl-2 mr-xl-2">
+        {/* Force next columns to break to new line */}
+        <div className="w-100 d-none d-md-block"></div>
+        {portfolioState.map((item) => (
+          <PortfolioTiles
+            key={item.name}
+            name={item.name}
+            image={item.image}
+            imageAlt={item.imageAlt}
+            site={item.site}
+            icon={item.icon}
+            iconAlt={item.iconAlt}
+            type={item.type}
+            github={item.github}
+          />
+        ))}
+      </div>
+    </section>
+  );
 }
 
-export default Portfolio
+export default Portfolio;
